25b5224279bbe97012f45de300575fc9022a872d
farukh110 <farukhsajjad110@gmail.com>
2024-12-03 23:40:37 +0500

    applied code splitting

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Home from './pages/Home/Home';
-import AboutSix from './pages/About/AboutSix';
-import Contact from './pages/Contact';
-import ServiceTwo from './pages/services/ServiceTwo';
-// import ServiceSix from './pages/services/ServiceSix/index';
-import PortfolioPage from './pages/Portfolio/PortfolioPage';
-import RequestDemo from './pages/RequestQuote';
-import WebDevelopment from './pages/services/inner-services/web-development';
-import LogoDesign from './pages/services/inner-services/logo-design';
-import Branding from './pages/services/inner-services/branding';
-import AppDevelopment from './pages/services/inner-services/app-development';
-import VideoAnimation from './pages/services/inner-services/video-animation';
-import ContentWriting from './pages/services/inner-services/content-writing';
-import Seo from './pages/services/inner-services/seo';
+
+// Lazy load the components
+const Home = lazy(() => import('./pages/Home/Home'));
+const AboutSix = lazy(() => import('./pages/About/AboutSix'));
+const Contact = lazy(() => import('./pages/Contact'));
+const ServiceTwo = lazy(() => import('./pages/services/ServiceTwo'));
+const PortfolioPage = lazy(() => import('./pages/Portfolio/PortfolioPage'));
+const RequestDemo = lazy(() => import('./pages/RequestQuote'));
+const WebDevelopment = lazy(() => import('./pages/services/inner-services/web-development'));
+const LogoDesign = lazy(() => import('./pages/services/inner-services/logo-design'));
+const Branding = lazy(() => import('./pages/services/inner-services/branding'));
+const AppDevelopment = lazy(() => import('./pages/services/inner-services/app-development'));
+const VideoAnimation = lazy(() => import('./pages/services/inner-services/video-animation'));
+const ContentWriting = lazy(() => import('./pages/services/inner-services/content-writing'));
+const Seo = lazy(() => import('./pages/services/inner-services/seo'));
 
 const App = () => {
-
   return (
     <>
       <Header />
-
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about-us" element={<AboutSix />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/services" element={<ServiceTwo />} />
-        <Route exact path="/services/web-development" element={<WebDevelopment />} />
-        <Route exact path="/services/logo-design" element={<LogoDesign />} />
-        <Route exact path="/services/branding" element={<Branding />} />
-        <Route exact path="/services/app-design-and-development" element={<AppDevelopment />} />
-        <Route exact path="/services/video-animation" element={<VideoAnimation />} />
-        <Route exact path="/services/content-writing" element={<ContentWriting />} />
-        <Route exact path="/services/seo" element={<Seo />} />
-        <Route exact path="/portfolio" element={<PortfolioPage />} />
-        <Route exact path="/get-free-quote" element={<RequestDemo />} />
-
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about-us" element={<AboutSix />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/services" element={<ServiceTwo />} />
+          <Route exact path="/services/web-development" element={<WebDevelopment />} />
+          <Route exact path="/services/logo-design" element={<LogoDesign />} />
+          <Route exact path="/services/branding" element={<Branding />} />
+          <Route exact path="/services/app-design-and-development" element={<AppDevelopment />} />
+          <Route exact path="/services/video-animation" element={<VideoAnimation />} />
+          <Route exact path="/services/content-writing" element={<ContentWriting />} />
+          <Route exact path="/services/seo" element={<Seo />} />
+          <Route exact path="/portfolio" element={<PortfolioPage />} />
+          <Route exact path="/get-free-quote" element={<RequestDemo />} />
+        </Routes>
+      </Suspense>
       <Footer />
-
     </>
-  )
+  );
 }
 
-export default App; 
+export default App;
