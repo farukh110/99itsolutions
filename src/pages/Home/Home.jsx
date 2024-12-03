bd9f42b2953fa22e6d8e1f23d5ee70e5f9b71794
farukh110 <farukhsajjad110@gmail.com>
2024-12-03 23:32:40 +0500

    i am updating few issues

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
-// import About from './components/About';
-// import Gallery from './components/Gallery';
-// import Hero from './components/Hero';
-// import Pricing from './components/Pricing';
-// import Services from './components/Services';
-// import Technologies from './components/Technologies';
-// import FullSlider from './components/FullSlider';
 import OurServices from './components/OurServices';
-// import OurItSolutions from './components/OurItSolutions';
-// import MainSlider from './components/MainSlider';
 import OurGalleries from './components/OurGalleries';
 import OurWork from './components/OurWork';
 import Counters from './components/Counters';
-import FibonacciSphere from './components/FibonacciSphere/FibonacciSphere';
-// import MainBanner from './components/MainBanner/MainBanner';
 import brain from '../../assets/images/services/brain1.jpg';
 import Focused from './components/Focused/Focused';
 import Process from './components/Process/Process';
@@ -50,9 +39,6 @@ const Home = () => {
 
     return (
         <>
-            {/* <MainBanner /> */}
-            {/* <FibonacciSphere /> */}
-            {/* <MainSlider /> */}
             <TextSlider />
             <OurServices />
             <Skills
@@ -67,8 +53,6 @@ const Home = () => {
             <Counters />
             <OurWork isLogoDesignPage={true} isWebPage={true} isMobilePage={true} isGraphicDesignPage={true} />
             <OurGalleries />
-            {/* <Work /> */}
-            {/* <Technologies /> */}
             <ContactForm customPadding="ps-0 pe-0" />
             <Clients />
         </>
