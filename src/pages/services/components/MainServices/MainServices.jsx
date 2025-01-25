afc9f0bd560258f17ec31676c2e70de89b7ab51c
farukh110 <farukhsajjad110@gmail.com>
2025-01-25 19:29:32 +0500

    updated services page

--- src/pages/services/components/MainServices/MainServices.jsx
+++ src/pages/services/components/MainServices/MainServices.jsx
@@ -38,6 +38,154 @@ const MainServices = () => {
                                         >
                                             <div className="ag-courses-item_bg" />
                                             <div className="ag-courses-item_title ag-courses-item_title-index">
+                                                Custom Software Development
+                                            </div>
+                                            <div className="ag-courses-item_date-box pb-3">
+                                                <p>
+                                                    We specialize in creating customized software solutions designed to tackle your unique business challenges. Our scalable and efficient systems are crafted to streamline operations and enhance functionality.
+
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-2">
+                            <div className="ag-format-container">
+                                <div className="ag-courses_box">
+                                    <div className="ag-courses_item">
+                                        <a
+                                            href="ai-ml-development"
+                                            aria-label="AI/ML Software Development"
+                                            className="ag-courses-item_link ag-courses-item_link-index"
+                                            target="_blank"
+                                        >
+                                            <div className="ag-courses-item_bg" />
+                                            <div className="ag-courses-item_title ag-courses-item_title-index">
+                                                AI & ML Development
+                                            </div>
+                                            <div className="ag-courses-item_date-box pb-3">
+                                                <p>
+                                                    Harness the power of Artificial Intelligence and Machine Learning with our innovative solutions. From process automation to smarter decision-making, our AI/ML expertise empowers your business to achieve new heights.
+
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-2">
+                            <div className="ag-format-container">
+                                <div className="ag-courses_box">
+                                    <div className="ag-courses_item">
+                                        <a
+                                            href="mobile-app-development"
+                                            aria-label="Mobile App Development"
+                                            className="ag-courses-item_link ag-courses-item_link-index"
+                                        >
+                                            <div className="ag-courses-item_bg" />
+                                            <div className="ag-courses-item_title ag-courses-item_title-index">
+                                                Mobile App Development
+                                            </div>
+                                            <div className="ag-courses-item_date-box pb-3">
+                                                <p>
+                                                    Transform your business with user-friendly and feature-rich mobile applications. Our apps are designed to enhance user engagement and boost your digital presence, ensuring top-notch performance and scalability.
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-2">
+                            <div className="ag-format-container">
+                                <div className="ag-courses_box">
+                                    <div className="ag-courses_item">
+                                        <a
+                                            href="software-product-development"
+                                            aria-label="Software Product Development"
+                                            className="ag-courses-item_link ag-courses-item_link-index"
+                                            target="_blank"
+                                        >
+                                            <div className="ag-courses-item_bg" />
+                                            <div className="ag-courses-item_title ag-courses-item_title-index">
+                                                Software Product Development
+                                            </div>
+                                            <div className="ag-courses-item_date-box pb-3">
+                                                <p>
+                                                    From concept to launch, we build robust and market-ready software products that align with your business objectives. Our goal is to create long-term value with every solution.
+
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-2">
+                            <div className="ag-format-container">
+                                <div className="ag-courses_box">
+                                    <div className="ag-courses_item">
+                                        <a
+                                            href="hire-dedicated-developer"
+                                            aria-label=" Hire Dedicated Developers"
+                                            className="ag-courses-item_link ag-courses-item_link-index"
+                                            target="_blank"
+                                        >
+                                            <div className="ag-courses-item_bg" />
+                                            <div className="ag-courses-item_title ag-courses-item_title-index">
+                                                Hire Dedicated Developers
+                                            </div>
+                                            <div className="ag-courses-item_date-box pb-3">
+                                                <p>
+                                                    Gain access to highly skilled developers dedicated to your project’s success. Our professionals bring focused expertise, ensuring high-quality results that meet your specific needs.
+
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-2">
+                            <div className="ag-format-container">
+                                <div className="ag-courses_box">
+                                    <div className="ag-courses_item">
+                                        <a
+                                            href="it-outsourcing"
+                                            aria-label="IT Outsourcing Services"
+                                            className="ag-courses-item_link ag-courses-item_link-index"
+                                            target="_blank"
+                                        >
+                                            <div className="ag-courses-item_bg" />
+                                            <div className="ag-courses-item_title ag-courses-item_title-index">
+                                                IT Outsourcing Services
+                                            </div>
+                                            <div className="ag-courses-item_date-box pb-3">
+                                                <p>
+                                                    Streamline your operations and enhance scalability with our comprehensive IT outsourcing services. Acting as an extension of your team, we help you achieve operational efficiency and achieve your goals seamlessly.
+
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-2">
+                            <div className="ag-format-container">
+                                <div className="ag-courses_box">
+                                    <div className="ag-courses_item">
+                                        <a
+                                            href="software-development"
+                                            aria-label="Custom Software Development"
+                                            className="ag-courses-item_link ag-courses-item_link-index"
+                                            target="_blank"
+                                        >
+                                            <div className="ag-courses-item_bg" />
+                                            <div className="ag-courses-item_title ag-courses-item_title-index">
                                                 Domain and Hosting
                                             </div>
                                             <div className="ag-courses-item_date-box pb-3">
