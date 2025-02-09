71fb59439dd66a2864cd039c8fcd37089e4cea66
farukh110 <farukhsajjad110@gmail.com>
2025-02-09 23:12:18 +0500

    updated few things

--- src/pages/OutSourcing/OutSourcing.jsx
+++ src/pages/OutSourcing/OutSourcing.jsx
@@ -8,6 +8,7 @@ import ContactForm from '../Home/components/ContactForm/ContactForm';
 import VerticalSlider from '../Home/components/vertical-slider/VerticalSlider';
 import Technologies from '../Home/components/Techologies/Technologies';
 import OutSourcingService from './components/OutSourcingService/OutSourcingService';
+import CustomHeading from '../../components/heading/CustomHeading';
 
 const OutSourcing = () => {
     return (
@@ -20,7 +21,25 @@ const OutSourcing = () => {
             <Technologies />
             <ContactForm customPadding="ps-0 pe-0" />
             <OutSourcingService />
+
+            <div className='container mb-md-5'>
+                <CustomHeading
+                    firstWord="99 IT Solutions - Your Trusted"
+                    lastWord="Technology Partner"
+                    textCenter="text-center"
+                />
+                <p>
+
+                    Since 2015, we have been running 99 IT Solutions that provides a full range of IT services, including Logo designing, domain, hosting, Animation Services, web designing and software development, digital marketing and customer support. We strive to provide the latest technology and best customer service to meet the needs of all my clients.
+                    We have a team of professional developers, designers and digital marketers who have great experience and expertise in their respective fields. We have a good track record of successfully completing the projects on time and within the budget. We focus on quality work and customer satisfaction. We also provide training and support to our customers.
+                    <br /><br />
+                    We are continuously striving to maintain our place in the market and stay ahead of the competition by providing excellent services to our clients. We are always looking for ways to improve our services and provide the best service to our clients.
+                    <br /><br />
+                    To boost the business, We also provide some digital marketing services to our clients. We have expertise in SEO, PPC, Content Marketing, Social Media Marketing and Email Marketing services. We provide complete digital marketing solutions for our clients.
+                </p>
+            </div>
             <VerticalSlider />
+
         </>
     )
 }
